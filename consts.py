ed8c733d8e64f40745787e380e2ee3dd2209c1ed
avishaigretz <avishaig@il.ibm.com>
2022-12-04 13:03:58 +0200

    adding assessment.py and dialog_acts.csv updating consts.py

--- consts.py
+++ consts.py
@@ -9,10 +9,10 @@ MODEL_DIR = 'dialog_act_model'
 CLASSES_FILE = 'dialog_acts.csv'
 CLASSES_PATH = os.path.join(DATASET_DIR, CLASSES_FILE)
 
-# BASE_MODEL = 'roberta-large'
-# N_EPOCHS = 15
-BASE_MODEL = 'roberta-base'
-N_EPOCHS = 1
+BASE_MODEL = 'roberta-large'
+N_EPOCHS = 15
+# BASE_MODEL = 'roberta-base'
+# N_EPOCHS = 1
 LEARNING_RATE = 5e-6
 
 HF_DATASET_ID = "ibm/vira-dialog-acts-live"
