ecdafa4eab1acdf124636324713a34b2f9f847f6
Assaf Toledo <assaf.toledo@ibm.com>
2023-08-02 14:49:11 +0300

    fixes for integration with dialog system

--- service.py
+++ service.py
@@ -5,24 +5,26 @@
 
 import logging.config
 import os
+from typing import List
 
 import numpy as np
 import pandas as pd
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 
 from consts import HF_MODEL_ID, CLASSES_PATH
 
 logging.config.fileConfig('logging.conf')
-log = logging.getLogger('services.dialog')
+log = logging.getLogger('main')
 
 log.info('Initiating service')
 app = FastAPI(openapi_url=None)
 
 tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_ID)
 model = AutoModelForSequenceClassification.from_pretrained(HF_MODEL_ID)
-pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=True)
+pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer, top_k=None)
 df = pd.read_csv(CLASSES_PATH)
 model_trained_classes = df.iloc[:, 0].apply(str.strip).to_list()
 
@@ -31,11 +33,11 @@ log.info('Service is ready')
 
 def get_model_predictions(candidates):
     results = pipeline(candidates)
-    class_scores = np.array([[label['score'] for label in result] for result in results])
-    class_ids = np.argmax(class_scores, axis=1)
-    class_scores = np.max(class_scores, axis=1)
-    model_classes = [model_trained_classes[class_id] for class_id in class_ids]
-    return model_classes, class_scores.tolist()
+    intents = [[model_trained_classes[int(label['label'][6:])] for label in result]
+               for result in results]
+    scores = [[label['score'] for label in result]
+              for result in results]
+    return intents, scores
 
 
 @app.get("/")
@@ -44,14 +46,30 @@ def read_root():
 
 
 @app.get("/health")
-def read_root():
+def health():
     return "OK"
 
 
-@app.get("/classify")
-def read_root(text: str):
-    return get_model_predictions([text])
+class RequestModel(BaseModel):
+    text: str
+
+
+class ResponseModel(BaseModel):
+    intents: List[str]
+    scores: List[float]
+
+
+@app.post("/classify", response_model=ResponseModel)
+def classify(request: RequestModel):
+    intents, scores = get_model_predictions([request.text])
+    return ResponseModel(intents=intents[0], scores=scores[0])
+
+
+def main():
+    # intents, scores = classify("is the vaccine safe?")
+    # print(intents, scores)
+    uvicorn.run(app, host="0.0.0.0", port=8120)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8040)
+    main()
